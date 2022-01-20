2f786baf2e04cd7fc53087774030efd408c9e5a5
Rvydkybs <rvyd.kybs@outlook.com>
2022-01-20 15:35:23 +0200

    components

--- patikaone/src/App.js
+++ patikaone/src/App.js
@@ -2,25 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import myFonk from './homeWork';//import edilen fonksiyon
 
-
+const name="rüveyda";
+const isLoogedIn=false;
+const notLogin=false;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <h1>{name}</h1>
+      <h1>{`my name is ${name}`}</h1>
+      {isLoogedIn && " giriş yaptı"} <br></br>
+      {!notLogin&& "giriş yapılmadı"}
+      <br></br>
+      {isLoogedIn?"my name is rüveyda":!notLogin&& "giriş yapılmadı"} 
+   </>
   );
 }
 myFonk(5);
